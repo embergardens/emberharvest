5ca37151bcbd02b249929c07c032fc05df45c831
Tim Spears <timspears@imarc.com>
2021-11-27 16:47:37 -0500

    add acf options and locations post type

--- functions.php
+++ functions.php
@@ -7,47 +7,83 @@
 
 add_theme_support( 'post-thumbnails' );
 
+// ADD ACF OPTIONS PAGE.
+if ( function_exists( 'acf_add_options_page' ) ) {
+	acf_add_options_page(
+		array(
+			'page_title' => 'Site Options',
+			'menu_title' => 'Options',
+			'menu_slug'  => 'site-options',
+			'capability' => 'edit_posts',
+			'redirect'   => true,
+			'position'   => '1',
+		)
+	);
+
+	acf_add_options_sub_page(
+		array(
+			'page_title'  => 'Global Options',
+			'menu_title'  => 'Global',
+			'parent_slug' => 'site-options',
+		)
+	);
+
+	acf_add_options_sub_page(
+		array(
+			'page_title'  => 'Navigation Options',
+			'menu_title'  => 'Navigation',
+			'parent_slug' => 'site-options',
+		)
+	);
+
+}
+
+// REMOVE MENUS FROM ADMIN.
 function remove_menus() {
-	remove_menu_page( 'index.php' ); // Dashboard.
+	// remove_menu_page( 'index.php' ); // Dashboard.
 	remove_menu_page( "jetpack" ); // Jetpack.
 	remove_menu_page( "edit-comments.php" ); // Comments.
 }
 add_action("admin_menu", "remove_menus");
 
+// REORDER MENUS IN ADMIN.
 function headless_custom_menu_order( $menu_ord ) {
 	if ( ! $menu_ord ) return true;
 
 	return array(
-		"edit.php?post_type=page", // Pages
-		"edit.php", // Posts
-		"edit.php?post_type=custom_posts", // Custom Post Type
-		"separator1", // First separator
+		'index.php',
+		'edit.php?post_type=page', // Pages.
+		'edit.php?post_type=location', // Locations.
+		'edit.php', // Posts.
+		'separator1', // First separator.
 
-		"upload.php", // Media
-		"themes.php", // Appearance
-		"plugins.php", // Plugins
-		"users.php", // Users
-		"separator2", // Second separator
+		'upload.php', // Media.
+		'admin.php?page=acf-options-global',
+		'themes.php', // Appearance.
+		'plugins.php', // Plugins.
+		'users.php', // Users.
+		'separator2', // Second separator.
 
-		"tools.php", // Tools
-		"options-general.php", // Settings
-		"separator-last", // Last separator
+		'tools.php', // Tools.
+		'options-general.php', // Settings.
+		'separator-last', // Last separator.
 	);
 }
-add_filter("custom_menu_order", "headless_custom_menu_order", 10, 1);
-add_filter("menu_order", "headless_custom_menu_order", 10, 1);
+add_filter( 'custom_menu_order', 'headless_custom_menu_order', 10, 1 );
+add_filter( 'menu_order', 'headless_custom_menu_order', 10, 1 );
 
+// DISABLE RSS FEED.
 function headless_disable_feed() {
-	wp_die(__('No feed available, please visit our <a href="' . get_bloginfo("url") . '">homepage</a>!'));
+	wp_die(__( 'No feed available, please visit our <a href="' . get_bloginfo("url") . '">homepage</a>!' ) );
 }
 
-add_action("do_feed", "headless_disable_feed", 1);
-add_action("do_feed_rdf", "headless_disable_feed", 1);
-add_action("do_feed_rss", "headless_disable_feed", 1);
-add_action("do_feed_rss2", "headless_disable_feed", 1);
-add_action("do_feed_atom", "headless_disable_feed", 1);
-add_action("do_feed_rss2_comments", "headless_disable_feed", 1);
-add_action("do_feed_atom_comments", "headless_disable_feed", 1);
+add_action( 'do_feed', 'headless_disable_feed', 1 );
+add_action( 'do_feed_rdf', 'headless_disable_feed', 1 );
+add_action( 'do_feed_rss', 'headless_disable_feed', 1 );
+add_action( 'do_feed_rss2', 'headless_disable_feed', 1 );
+add_action( 'do_feed_atom', 'headless_disable_feed', 1 );
+add_action( 'do_feed_rss2_comments', 'headless_disable_feed', 1 );
+add_action( 'do_feed_atom_comments', 'headless_disable_feed', 1 );
 
 // Return `null` if an empty value is returned from ACF.
 if (!function_exists("acf_nullify_empty")) {
@@ -59,4 +95,22 @@ if (!function_exists("acf_nullify_empty")) {
 		return $value;
 	}
 }
-add_filter("acf/format_value", "acf_nullify_empty", 100, 3);
+add_filter( 'acf/format_value', 'acf_nullify_empty', 100, 3 );
+
+// INCLUDE POST TYPES
+require get_template_directory() . '/post-types/locations.php';
+
+/**
+ * Register navigation menus uses wp_nav_menu in multiple places.
+ */
+function emberharvest_menus() {
+
+	$locations = array(
+		'primary'    => __( 'Primary Navigation', 'emberharvest' ),
+		'social'     => __( 'Social Menu', 'emberharvest' ),
+	);
+
+	register_nav_menus( $locations );
+}
+
+add_action( 'init', 'emberharvest_menus' );
---
+++ post-types/locations.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Custom Post Type - Locations
+ *
+ * @package emberharvest
+ */
+
+function emberharvest_locations() {
+	$labels = array(
+		'name'               => __( 'Locations', 'emberharvest' ),
+		'singular_name'      => __( 'Location', 'emberharvest' ),
+		'add_new'            => __( 'Add New', 'emberharvest' ),
+		'add_new_item'       => __( 'Add New Location', 'emberharvest' ),
+		'edit'               => __( 'Edit', 'emberharvest' ),
+		'edit_item'          => __( 'Edit Location', 'emberharvest' ),
+		'new_item'           => __( 'New Location', 'emberharvest' ),
+		'view'               => __( 'View Location', 'emberharvest' ),
+		'view_item'          => __( 'View Location', 'emberharvest' ),
+		'search_items'       => __( 'Search Locations', 'emberharvest' ),
+		'not_found'          => __( 'No Locations found', 'emberharvest' ),
+		'not_found_in_trash' => __( 'No Locations found in Trash', 'emberharvest' ),
+		'parent'             => __( 'Parent Location', 'emberharvest' ),
+	);
+	$args  = array(
+		'label'               => __( 'Location', 'emberharvest' ),
+		'description'         => __( 'Location', 'emberharvest' ),
+		'labels'              => $labels,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'publicly_queryable'  => true,
+		'exclude_from_search' => false,
+		'menu_position'       => 1,
+		'menu_icon'           => 'dashicons-location-alt',
+		'hierarchical'        => true,
+		'query_var'           => true,
+		'capability_type'     => 'page',
+		'supports'            => array( 'title', 'page-attributes', 'thumbnail' ),
+		'has_archive'         => true,
+		'rewrite'             => array( 'slug' => 'locations', 'with_front' => false ),
+		'taxonomies'          => array(),
+		'can_export'          => true,
+	);
+	register_post_type( 'location', $args );
+
+}
+add_action( 'init', 'emberharvest_locations' );
